ed8d7983673fe122fe562dff5be14e1e4043453b
Pau <pablo.sanchez@mediaadgo.com>
2020-01-13 12:40:41 +0100

    first commit

---
+++ src/domain/user/User.ts
@@ -0,0 +1,25 @@
+import {UserId} from './UserId';
+
+export class User{
+    private _id: UserId;
+    private _name: UserName;
+    private _email: UserEmail;
+
+    constructor(id: UserId, name: UserName, email: UserEmail) {
+        this._id    = id;
+        this._name  = name;
+        this._email = email;
+    }
+
+    get id(): UserId {
+        return this._id;
+    }
+
+    get name(): UserName {
+        return this._name;
+    }
+
+    get email(): UserEmail {
+        return this._email;
+    }
+}
